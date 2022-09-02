733945aae07ff2442c1a7502dac637295127bf8b
Ilsa Irfan <98927393+ilsairfan@users.noreply.github.com>
2022-09-02 14:23:52 +0200

    Revert "'pokemonList' variable created"
    This reverts commit 9cc6f2e76cf44e8c4cee65abb836cbd2fe7234f1.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-pokemonList = [
-  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-  { name: 'Butterfree', height: 11, types: ['bug', 'free'] },
-  { name: 'Jigglypuff', height: 5, types: ['fairy', 'normal'] },
-];
+alert('Hello World');
+
+let favoriteFood = 'Burgers';
+document.write(favoriteFood);
