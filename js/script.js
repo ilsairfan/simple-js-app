b75727298cca8c5fb71287b4afa2f9d740f78fd3
Ilsa Irfan <98927393+ilsairfan@users.noreply.github.com>
2022-10-05 15:05:19 +0200

    From for loop to forEach function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 //An array is assigned to the new variable created. Several objects are then added to an array.
+
 let pokemonList = [
   { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
   { name: 'Butterfree', height: 1.1, types: ['bug', 'free'] },
   { name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal'] },
 ];
 
-//for loop created to iterate over each item in the variable
-for (i = 0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+//forEach loop created to iterate over each item in the variable
 
+pokemonList.forEach((pokemon) => {
+  document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
   //Conditional added within the loop.
 
-  if (pokemonList[i].height <= 0.5){
-    document.write('- It is a small-sized Pokemon. ');
+  if (pokemon.height <= 0.5){
+    document.write('- It is a small-sized Pokemon. ' + '<br>');
   }
-  else if (pokemonList[i].height > 0.5 && pokemonList[i].height <= 1.0) {
-    document.write('- It is an average-sized Pokemon. ');
+  else if (pokemon.height > 0.5 && pokemon.height <= 1.0) {
+    document.write('- It is an average-sized Pokemon. ' + '<br>');
   }
   else {
-    document.write('- Wow, it is a big Pokemon. ');
+    document.write('- Wow, it is a big Pokemon. ' + '<br>');
   }
-}
+
+});
