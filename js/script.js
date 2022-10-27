068b278c21ccf2a5eb56878f903512079d2c8fbd
Ilsa Irfan <98927393+ilsairfan@users.noreply.github.com>
2022-10-27 18:40:41 +0200

    DOM Manipulation and Event Listener
    1) <ul> element created in HTML
    2) Styling added to each Pokemon Button
    3) Button element created and event listener function is added.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-//Part 1 of Task 1.5
-/*let pokemonList = [
-  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-  { name: 'Butterfree', height: 1.1, types: ['bug', 'free'] },
-  { name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal'] },
-];
-
-//forEach loop created to iterate over each item in the variable
-
-pokemonList.forEach((pokemon) => {
-  document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
-  //Conditional added within the loop.
-
-  if (pokemon.height <= 0.5) {
-    document.write('- It is a small-sized Pokemon. ' + '<br>');
-  }
-  else if (pokemon.height > 0.5 && pokemon.height <= 1.0) {
-    document.write('- It is an average-sized Pokemon. ' + '<br>');
-  }
-  else {
-    document.write('- Wow, it is a big Pokemon. ' + '<br>');
-  }
-
-});*/
-
-//Part 2
-
 //entire code is wrapped inside an IIFE to avoid any clashes with the external code.
 
 let pokemonRepository = (function() {
@@ -43,43 +16,45 @@ let pokemonRepository = (function() {
     pokemonList.push(pokemon);
   }
 
-  //Attempted bonus task 1.5. Not sure about this code right now.
-  /*
-  function addv(pokemon) {
-    if (typeof pokemon === 'object' && Object.keys(pokemonList[0]) === Object.keys(pokemon)) {
-      add(pokemon)
-    }
-    else {
-      document.write('Data type is not correct')
-    }
-  }*/
+//document.write is removed from forEach loop
+
+  function addListItem(pokemon){
+
+    //Button created for each Pokemon name
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+
+    //Event listener is added that will listen to 'click'
+    button.addEventListener('click', showDetails(pokemon))
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon)
+  }
+
 
   //return is an object itself with key value pairs
   return {
     getAll: getAll,
     add: add,
-    //addv: addv
+    addListItem: addListItem
   }
 
 })();
 
-pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: 'electric' });
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: 'electric'});
+//pokemonRepository.addv('Pikachu')
 //document.write(pokemonRepository.getAll());
 
 //Due to IIFE pokemon list has limited access, loop code is updated to access the list.
 
-pokemonRepository.getAll().forEach((pokemon) => {
-  document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
-  //Conditional added within the loop.
+pokemonRepository.getAll().forEach(function (pokemon){
 
-  if (pokemon.height <= 0.5) {
-    document.write('- It is a small-sized Pokemon. ' + '<br>');
-  }
-  else if (pokemon.height > 0.5 && pokemon.height <= 1.0) {
-    document.write('- It is an average-sized Pokemon. ' + '<br>');
-  }
-  else {
-    document.write('- Wow, it is a big Pokemon. ' + '<br>');
-  }
+  pokemonRepository.addListItem(pokemon);
 
 });
